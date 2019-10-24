95b3e42c6b84273f9e6b0438ed0090743c81957f
Alex Gustafsson <alex-gustafsson@live.se>
2019-10-24 12:29:36 +0200

    Added services to the config, renamed structs for sake of clarity

--- core/config.go
+++ core/config.go
@@ -1,7 +1,7 @@
 package core
 
-// Peer describes the configuration for a peer in the network
-type Peer struct {
+// PeerConfig describes the configuration for a peer in the network
+type PeerConfig struct {
   Certificate string
   Key string
   Fingerprint string
@@ -9,8 +9,18 @@ type Peer struct {
   Port int
 }
 
+// ServiceConfig describes a service being monitored
+type ServiceConfig struct {
+  Hostname string
+  Port int
+  // Checks is an array of module names used for checking. The modules are run
+  // in order. If one fails, the next is run.
+  Checks []string
+}
+
 // Config describes a configuration used for Upmon
 type Config struct {
   LogLevel string
-  Peers map[string]Peer
+  Peers map[string]PeerConfig
+  Services map[string]ServiceConfig
 }
