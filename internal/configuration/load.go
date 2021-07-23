a8bdfb9d723d47c3485cff2a48705d612ce2750b
Alex Gustafsson <alex-gustafsson@live.se>
2021-07-23 12:11:47 +0200

    Fix incorrect default address

--- internal/configuration/load.go
+++ internal/configuration/load.go
@@ -14,7 +14,7 @@ func Load(filePath string) (*Configuration, error) {
 	k := koanf.New(".")
 
 	defaults := confmap.Provider(map[string]interface{}{
-		"address":     "",
+		"address":     "127.0.0.1",
 		"port":        "7070",
 		"api.enabled": false,
 	}, ".")
