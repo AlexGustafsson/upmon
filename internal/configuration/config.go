49a0a3b73ca4606bbcad7f27c6756d46bf9ad201
Alex Gustafsson <alex-gustafsson@live.se>
2021-07-23 08:16:44 +0200

    Implement service and check configuration

--- internal/configuration/config.go
+++ internal/configuration/config.go
@@ -2,17 +2,30 @@ package configuration
 
 import "github.com/hashicorp/memberlist"
 
-type Peer struct {
+type PeerConfiguration struct {
 	Address string `koanf:"address"`
 	Port    uint16 `koanf:"port"`
 }
 
+type CheckConfiguration struct {
+	Type        string                 `koanf:"type"`
+	Name        string                 `koanf:"name"`
+	Description string                 `koanf:"description"`
+	Options     map[string]interface{} `koanf:"options"`
+}
+
+type ServiceConfiguration struct {
+	Description string               `koanf:"description"`
+	Checks      []CheckConfiguration `koanf:"checks"`
+}
+
 type Configuration struct {
-	Name    string `koanf:"name"`
-	Address string `koanf:"address"`
-	Port    uint16 `koanf:"port"`
-	Peers   []Peer `koanf:"peers"`
-	Api     struct {
+	Name     string                          `koanf:"name"`
+	Address  string                          `koanf:"address"`
+	Port     uint16                          `koanf:"port"`
+	Peers    []PeerConfiguration             `koanf:"peers"`
+	Services map[string]ServiceConfiguration `koanf:"services"`
+	Api      struct {
 		Enabled bool   `koanf:"enabled"`
 		Address string `koanf:"address"`
 		Port    uint16 `koanf:"port"`
